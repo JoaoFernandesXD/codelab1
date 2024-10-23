4471c13ac9a2a368b3f98dbce776fa6ccf2e32cb
João <joaofernandesxd1@icloud.com>
2024-10-23 20:15:43 -0300

    [feat] - adicionando props no card

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -7,7 +7,7 @@ import Card from '../Card/Card';
 function Main(){
     return(
         <main>
-            <Card></Card>
+            <Card data='17 de ago, 2024' titulo='O que é linguagem de programação? Conheça as principais' texto='Uma das mais populares vertentes da tecnologia da informação, a área de programação segue tendo muita demanda de trabalho justamente pela velocidade com que dispositivos tecnológicos vêm avançando.'></Card>
         </main>
     )
 }
